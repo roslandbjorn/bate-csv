5740e4f733c78696c117f8c7330598941fac90ed
bjorn-rosland_adno <bjorn.rosland@advania.no>
2025-07-14 16:13:28 +0200

    removed set charset

--- src/utils/DownloadUtil.js
+++ src/utils/DownloadUtil.js
@@ -1,7 +1,7 @@
 const downloadFile = (data, fileName) => {
   const blob = new Blob([data]);
   const link = document.createElement("a");
-  link.href = URL.createObjectURL(blob, { type: "text/csv;charset=utf-8;" });
+  link.href = URL.createObjectURL(blob, { type: "text/csv;" });
   link.setAttribute("download", fileName);
   document.body.appendChild(link);
   link.click();
