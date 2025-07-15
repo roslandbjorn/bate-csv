2bd7a7604c6586e8a0930687ad71e833a5c5db45
bjorn-rosland_adno <bjorn.rosland@advania.no>
2025-07-15 10:37:21 +0200

    auto download file

--- src/components/FileUploadHandler.jsx
+++ src/components/FileUploadHandler.jsx
@@ -1,34 +1,26 @@
-import { useState } from "react";
 import convertToCsv from "../utils/CsvConverter";
 import downloadFile from "../utils/DownloadUtil";
 
 const FileUploadHandler = () => {
-  const [file, setFile] = useState({ data: null, name: "" });
-
-  const handleFileChange = (e) => {
+  const handleConvert = (e) => {
     if (e.target.files) {
       const file = e.target.files[0];
-      setFile({ data: file, name: file.name });
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const arrayBuffer = event.target.result;
+        const text = new TextDecoder("iso-8859-1").decode(arrayBuffer);
+        const csvData = convertToCsv(text);
+        const fileName = file.name.replace(".txt", ".csv");
+        downloadFile(csvData, fileName);
+      };
+      reader.readAsArrayBuffer(file);
     }
-  };
-
-  const handleConvert = () => {
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      const arrayBuffer = event.target.result;
-      const text = new TextDecoder("iso-8859-1").decode(arrayBuffer);
-      const csvData = convertToCsv(text);
-      const fileName = file.name.replace(".txt", ".csv");
-      downloadFile(csvData, fileName);
-    };
-    reader.readAsArrayBuffer(file.data);
   };
 
   return (
     <div>
       <h1>Konverter til CSV</h1>
-      <input type="file" onChange={handleFileChange} accept=".txt" />
-      <button onClick={handleConvert}>Konverter</button>
+      <input type="file" onChange={handleConvert} accept=".txt" />
     </div>
   );
 };
